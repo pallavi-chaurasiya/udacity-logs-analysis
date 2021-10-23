801b44df5fcfbca56804fc965d4c008bd1db7544
ckmohitsehra <93056662+ckmohitsehra@users.noreply.github.com>
2021-10-24 02:10:23 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 # Logging helper utility
 
 # Reference prefixes
-prefixes = ["Verbose", "Debug", ]
+prefixes = ["Verbose", "Debug", "Info", ]
 
 def print_log(prefix, log):
   print(prefix + " ", log)
