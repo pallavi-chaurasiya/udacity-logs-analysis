afbeeae088bac192ccaf5670fa33732e571e1d88
pallavi-chaurasiya <60112513+pallavi-chaurasiya@users.noreply.github.com>
2022-10-29 04:41:01 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 # Logging helper utility
 
 # Reference prefixes
-prefixes = ["Verbose", "Debug", "ExcessInfo", "Warn", "FatalError", "CriticalError",]
+prefixes = ["Verbose", "Debug", "ExcessInfo", "MinimalInfo", "Warn", "FatalError", "CriticalError",]
 
 def print_log(prefix, log):
   print(prefix + " ", log)
